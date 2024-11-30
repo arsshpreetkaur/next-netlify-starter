0d784e376e8a41dcfc64378f04d5a4ca908a122a
arsshpreetkaur <133889383+arsshpreetkaur@users.noreply.github.com>
2024-11-30 17:22:29 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,11 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+// pages/index.js
+import Layout from './components/Layout.js' ;
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
+    <Layout>
+      <h1>Welcome to Our Static Site</h1>
+      <p>We offer the best services for your business.</p>
+    </Layout>
+  );
 }
